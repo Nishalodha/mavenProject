ed4d07994a67119ba5afa961171734b3866fe159
Nisha <nishalodha21@gmail.com>
2020-05-19 15:18:59 +0530

    First commit

---
+++ Mavenjava/src/test/java/qaclickacademy/seleniumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class seleniumTest {
+
+	@Test(groups= {"smoke"})
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+	}
+	@Test
+	public void elementUi()
+	{
+		System.out.println("elementUi");
+	}
+}
+
